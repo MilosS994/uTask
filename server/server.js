79ee45eb5246117edef1d90f73c77cc07b8a5142
Milos Srejic <srejic.milos94@gmail.com>
2025-04-01 21:33:11 +0200

    feat: Improve server startup and database connection handling

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import connectDB from "./database/mongodb.js";
 import cors from "cors";
+import { PORT } from "./config/env.js";
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -10,6 +13,17 @@ app.get("/", (req, res) => {
   res.send("Hello from the server");
 });
 
-app.listen(5500, () => {
-  console.log("Server is running on port 5500");
-});
+// Start a server
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start a server: ", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
