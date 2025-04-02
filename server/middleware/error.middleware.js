b7067beb6a50045b13147fcb34a28240980df680
Milos Srejic <srejic.milos94@gmail.com>
2025-04-02 21:45:27 +0200

    feat: Implement global error handling middleware

---
+++ server/middleware/error.middleware.js
@@ -0,0 +1,28 @@
+const errorMiddleware = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Server error";
+
+  // Mongoose bad ObjectId
+  if (err.name === "CastError") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    statusCode = 400;
+    message = "Duplicate field value entered";
+  }
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    statusCode = 400;
+    message = Object.values(err.errors)
+      .map((val) => val.message)
+      .join(", ");
+  }
+
+  res.status(statusCode).json({ success: false, message: message });
+};
+
+export default errorMiddleware;
