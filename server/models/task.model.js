c656837a6c857d14e08285a945b9b318161778ee
Milos Srejic <srejic.milos94@gmail.com>
2025-04-02 21:38:32 +0200

    feat: Implement Task model with schema and validation

---
+++ server/models/task.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      minlength: [2, "Title must be at least 2 characters long"],
+      maxlength: [80, "Title can't be more than 80 characters long"],
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      maxlength: [500, "Description can't be more than 500 characters long"],
+      trim: true,
+    },
+
+    priority: {
+      type: Number,
+      enum: {
+        values: [1, 2, 3],
+        message:
+          "{VALUE} is not a supported priority level (must be 1, 2, or 3)",
+      },
+      default: 1,
+    },
+
+    done: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+taskSchema.index({ priority: 1, done: 1 });
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
