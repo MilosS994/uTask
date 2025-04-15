1e20613a0117d15c74cd8b11d3a82900a5111f5f
Milos Srejic <srejic.milos94@gmail.com>
2025-04-15 20:00:39 +0200

    feat: add profile modal to DashboardPage

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import { CgProfile } from "react-icons/cg";
+import Modal from "../components/app/Modal";
+import MyProfile from "../pages/MyProfile";
+// Zustand store for authentication
+import useAuthStore from "../store/authStore.js";
 
 const DashboardPage = () => {
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const user = useAuthStore((state) => state.user);
   return (
-    <div className="min-w-[100vw] min-h-[100vh] flex">
-      <aside className="w-[15%] bg-red-400"></aside>
-      <main className="flex-1 flex flex-col">
-        <div className="top h-[20%] bg-blue-300"></div>
-        <div className="bottom"></div>
-      </main>
-    </div>
+    <main className="min-w-[100vw] min-h-[100vh] flex flex-col">
+      <header className="flex justify-between items-center p-6 bg-blue-900 text-white border-b-4 border-yellow-600">
+        <h2 className="text-neutral-300 text-lg italic">
+          Welcome,{" "}
+          {user && (
+            <span className="text-yellow-600 font-semibold text-2xl ml-1">
+              {user.name}
+            </span>
+          )}
+        </h2>{" "}
+        <CgProfile
+          className="text-4xl hover:text-neutral-300 cursor-pointer"
+          onClick={() => setIsProfileOpen(true)}
+        />
+      </header>
+      {isProfileOpen && (
+        <Modal onClose={() => setIsProfileOpen(false)}>
+          <MyProfile />
+        </Modal>
+      )}
+      <section></section>
+      <footer></footer>
+    </main>
   );
 };
 
