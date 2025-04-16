2bf32f4c0926ac6d7a95fe43c8166974be97e6e5
Milos Srejic <srejic.milos94@gmail.com>
2025-04-16 19:46:42 +0200

    feat: Update Dashboard page design and structure with tasks grid component

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 // React Icons
 import { CgProfile } from "react-icons/cg";
 import { CiLogout } from "react-icons/ci";
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 import Modal from "../components/app/Modal";
 import MyProfile from "../pages/MyProfile";
 // Zustand store for authentication
 import useAuthStore from "../store/authStore.js";
+
+import TasksContainer from "../components/app/TasksContainer";
 
 const DashboardPage = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
@@ -15,22 +18,22 @@ const DashboardPage = () => {
   return (
     <main className="min-w-[100vw] min-h-[100vh] flex flex-col justify-between">
       {/* HEADER */}
-      <header className="flex justify-between items-center p-6 bg-blue-900 text-white border-b-4 border-yellow-600">
-        <h2 className="text-neutral-300 text-lg italic">
+      <header className="flex justify-between items-center p-6 bg-blue-900 text-white border-b-4 border-yellow-600 md:p-8 lg:p-10">
+        <h2 className="text-neutral-300 text-lg italic md:text-2xl lg:text-3xl">
           Welcome,{" "}
           {user && (
-            <span className="text-yellow-600 font-semibold text-2xl ml-1">
+            <span className="text-yellow-600 font-semibold text-2xl ml-1 md:text-3xl lg:text-4xl">
               {user.name}
             </span>
           )}
         </h2>{" "}
         <div className="flex gap-4 items-center">
           <CgProfile
-            className="text-4xl hover:text-neutral-300 cursor-pointer"
+            className="text-4xl hover:text-neutral-300 cursor-pointer md:text-5xl"
             onClick={() => setIsProfileOpen(true)}
           />
           <CiLogout
-            className="text-4xl hover:text-neutral-300 cursor-pointer"
+            className="text-4xl hover:text-neutral-300 cursor-pointer md:text-5xl"
             onClick={() => signout()}
           />
         </div>
@@ -41,12 +44,43 @@ const DashboardPage = () => {
           <MyProfile />
         </Modal>
       )}
-      <section className="flex-1"></section>
+      {/* TASKS SECTION */}
+      <section className="flex-1">
+        <div className="p-8 bg-blue-100 md:p-6 flex justify-evenly">
+          <IoIosAddCircleOutline className="text-4xl text-gray-800 hover:text-gray-600 cursor-pointer transition-all duration-100 ease-in-out md:text-5xl" />
+          <div className="flex items-center justify-center gap-2 md:gap-4 lg:gap-6">
+            <p className="text-md font-semibold text-gray-800 lg:text-lg">
+              Sort
+            </p>
+            <select
+              name="sort"
+              id="sort"
+              className="text-sm text-gray-800 cursor-pointer focus:outline-none focus:border-transparent rounded-md shadow-sm p-1 lg:text-lg"
+            >
+              <option value="default">Default</option>
+              <option value="low-high">Low to High</option>
+              <option value="high-low">High to Low</option>
+              <option value="done-first">Done</option>
+              <option value="active-first">Active</option>
+            </select>
+          </div>
+        </div>
+        <TasksContainer />
+      </section>
       {/* FOOTER */}
-      <footer>
-        <div className="p-4 text-center border-t-4 border-yellow-600">
-          <p className="font-light text-md text-gray-800">
+      <footer className="bg-blue-50">
+        <div className="p-4 text-center border-t-4 border-yellow-600 md:p-6 lg:p-8">
+          <p className="text-md text-gray-800 md:text-lg lg:text-xl">
             &copy; {new Date().getFullYear()} uTask. All rights reserved.
+          </p>
+          <p className="font-light text-md text-gray-800 md:text-lg lg:text-xl">
+            Need help?{" "}
+            <a
+              className="text-blue-800 hover:underline"
+              href="mailto:utaskapp.support@gmail.com"
+            >
+              utaskapp.support@gmail.com
+            </a>
           </p>
         </div>
       </footer>
