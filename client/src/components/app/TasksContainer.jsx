14dd99408eb0fcab46c96fa9780b995128f09fc3
Milos Srejic <srejic.milos94@gmail.com>
2025-04-16 19:46:04 +0200

    feat: Create tasks container grid

---
+++ client/src/components/app/TasksContainer.jsx
@@ -0,0 +1,27 @@
+import Task from "./Task";
+
+const TasksContainer = () => {
+  return (
+    <div className="w-full px-6 pb-6 flex flex-col gap-3 md:grid lg:grid-cols-2 lg:gap-6 lg:px-8 lg:pb-8 xl:px-10 xl:pb-10 bg-blue-100">
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+    </div>
+  );
+};
+
+export default TasksContainer;
