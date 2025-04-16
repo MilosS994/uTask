4546ae4b917205512cab87cae00aac929214a56e
Milos Srejic <srejic.milos94@gmail.com>
2025-04-16 22:12:39 +0200

    feat: Change container display to show No Tasks Yet if there are no tasks

--- client/src/components/app/TasksContainer.jsx
+++ client/src/components/app/TasksContainer.jsx
@@ -1,25 +1,37 @@
 import Task from "./Task";
+import useTaskStore from "../../store/taskStore.js";
+import { useEffect } from "react";
 
 const TasksContainer = () => {
+  const { tasks, getTasks, isLoading } = useTaskStore();
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-[60vh] flex justify-center items-center">
+        <p className="text-2xl text-gray-800">Loading...</p>
+      </div>
+    );
+  }
+
+  if (!isLoading && (!tasks || tasks.length === 0)) {
+    return (
+      <div className="w-full h-[30vh] flex justify-center items-center">
+        <p className="text-3xl text-gray-500 font-semibold text-center md:text-4xl lg:text-5xl">
+          No Tasks Yet
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div className="w-full px-6 pb-6 flex flex-col gap-3 md:grid lg:grid-cols-2 lg:gap-6 lg:px-8 lg:pb-8 xl:px-10 xl:pb-10 bg-blue-100">
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
+    <div className="w-full px-6 pb-6 flex flex-col gap-3 md:grid lg:grid-cols-2 lg:gap-6 lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
+      {tasks.map((task) => (
+        <Task key={task._id} task={task} />
+      ))}
     </div>
   );
 };
