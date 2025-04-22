568a439f3d04fcd3fb01fdfcd8e0bebf44dea5a9
Milos Srejic <srejic.milos94@gmail.com>
2025-04-22 18:18:35 +0200

    feat: Implement error message if there is no title entered

--- client/src/components/app/AddTaskForm.jsx
+++ client/src/components/app/AddTaskForm.jsx
@@ -9,10 +9,14 @@ const AddTaskForm = ({ onClose }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState(2);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!title.trim()) return;
+    if (!title.trim()) {
+      setErrorMessage("Title is required");
+      return;
+    }
 
     await createTask({ title, description, priority });
     setTitle("");
@@ -43,6 +47,9 @@ const AddTaskForm = ({ onClose }) => {
         <option value="2">Medium</option>
         <option value="1">Low</option>
       </select>
+      {errorMessage && (
+        <p className="text-red-700 font-semibold text-sm">{errorMessage}</p>
+      )}
       <Button type="submit" className="w-full cursor-pointer">
         Create Task
       </Button>
